21c84119572a6403e660828725eb574e93b11710
EsztiS <eszti@eszti.local>
2015-08-10 12:07:09 -0400

    .py version matches .ipynb version

--- raw_filter.py
+++ raw_filter.py
@@ -19,23 +19,7 @@ Signal are bandpass-filtered between 7 and 30 Hz to catch most of the signal of
 interest. 
 
 """
-
-print(__doc__)
-
-import numpy as np
-import pandas as pd
-from mne.io import RawArray
-from mne.channels import read_montage
-from mne.epochs import concatenate_epochs
-from mne import create_info, find_events, Epochs, concatenate_raws, pick_types
-from mne.decoding import CSP
-
-from sklearn.linear_model import LogisticRegression
-from glob import glob
-
-from scipy.signal import butter, lfilter, convolve, boxcar
-
-def creat_mne_raw_object(fname,read_events=True):
+def creat_mne_raw_object(fname,read_events=True,norm=False):
     """Create a mne raw instance from csv file"""
     # Read EEG file
     data = pd.read_csv(fname)
@@ -49,6 +33,11 @@ def creat_mne_raw_object(fname,read_events=True):
     ch_type = ['eeg']*len(ch_names)
     data = 1e-6*np.array(data[ch_names]).T # From microvolts to volts
     
+    # this normalizes the data per series.
+    if norm:
+        f = lambda x: (x - np.mean(x)) / np.std(x)
+        data = map(f,data)
+        
     if read_events:
         # events file
         ev_fname = fname.replace('_data','_events')
@@ -93,7 +82,7 @@ cols = ['HandStart','FirstDigitTouch',
 for subject in subjects:
 
     ################ READ DATA ################################################
-    fnames =  glob('/Users/eszti/Documents/__NYC_DSA/Dev/capstone/train/subj%d_series*_data.csv' % (subject))
+    fnames =  glob('/Users/eszti/Documents/__NYC_DSA/Dev/capstone_eeg/train/subj%d_series*_data.csv' % (subject))
     
     # read and concatenate all the files
     raw = concatenate_raws([creat_mne_raw_object(fname) for fname in fnames])
@@ -106,7 +95,9 @@ for subject in subjects:
     # with the rule of future data.
     # Here we use left filter compatible with this constraint. 
     raw._data[picks] = lfilter(b,a,raw._data[picks])
-    raw.df = raw.to_data_frame(index = None)
-    raw.df.to_csv('raw_filtered.csv', float_format='%.5f')
+    #raw.df = raw.to_data_frame(index = None)
+    #raw.df.to_csv('raw_filtered.csv', float_format='%.5f')
     
-    
+    # read ids
+    ids = np.concatenate([np.array(pd.read_csv(fname)['id']) for fname in fnames])
+    ids_tot.append(ids)
